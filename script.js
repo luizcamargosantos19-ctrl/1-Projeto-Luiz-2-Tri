f1af20c80f8b8f514513f80768303eaa3067a49f
tataquatroMCkevin <luiz.camargo.santos19@escola.pr.gov.br>
2026-06-15 08:50:08 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+package com.agrosoja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroSojaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgroSojaApplication.class, args);
+    }
+}
